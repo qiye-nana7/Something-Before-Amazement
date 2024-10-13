5f04ddc5e00d1272555b3b16e9e11139e612d404
Kurusaki_nanayo <2431695176@qq.com>
2024-10-13 14:54:33 +0800

    cs106l_initia

---
+++ CS106L/class/L1_stream/stream.cpp
@@ -0,0 +1,7 @@
+#include<iostream>// for cin, cout 
+#include<sstream>// for stringstream
+#include<chrono>// for *fancy* timers
+#include<iomanip>// for manipulators
+#include<fstream>// for file streams
+#include<vector>// for vector
+//#include<stream>
---
+++ CS106L/class/hello.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<sstream>
+#include<cstdio>
+#include<cstdlib>
+void assemble(int argc, char* argv);
+
+int main()
+{
+    
+    std::cout << "Hello,world!" << std::endl;
+    return 0;
+}
+
+void assemble(int argc, char* argv)
+{
+    asm( "sub $0x20,%rsp\n\t" // assembly code!
+        "movabs $0x77202c6f6c6c6548,%rax\n\t"
+        "mov %rax,(%rsp)\n\t"
+        "movl $0x646c726f, 0x8(%rsp)\n\t"
+        "movw $0x21, 0xc(%rsp)\n\t"
+        "movb $0x0,0xd(%rsp)\n\t"
+        "leaq (%rsp),%rax\n\t"
+        "mov %rax,%rdi\n\t"
+        "call __Z6myputsPc\n\t"
+        "add $0x20, %rsp\n\t"
+    );
+}
